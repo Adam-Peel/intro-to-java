1b059ba177296d15b45802691b2255fc10415d0b
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-09-01 17:45:50 +0100

    Update repo with chapter 4 exercises - angles of a random traingle

--- ch4/Exercises.java
+++ ch4/Exercises.java
@@ -8,6 +8,8 @@ public class Exercises {
     System.out.println(pentagonArea());
     System.out.println("Exercise 4.2 - Geometry: Great circle distance");
     System.out.println(greatCircleDistance());
+    System.out.println("Exercise 4.6 - Random points on a circle");
+    System.out.println(randomPoints());
   }
 
   public static String pentagonArea() {
@@ -30,5 +32,36 @@ public class Exercises {
     double earthRadius = 6371.01;
     double distance = earthRadius * Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
     return "The distance between the two points is: " + distance + " km.";
+  }
+
+  public static String randomPoints() {
+    double radius = 40.0;
+    double alpha1 = Math.random() * 2 * Math.PI;
+    double x1 = radius * Math.cos(alpha1);
+    double y1 = radius * Math.sin(alpha1);
+    double alpha2 = Math.random() * 2 * Math.PI;
+    double x2 = radius * Math.cos(alpha2);
+    double y2 = radius * Math.sin(alpha2);
+    double alpha3 = Math.random() * 2 * Math.PI;
+    double x3 = radius * Math.cos(alpha3);
+    double y3 = radius * Math.sin(alpha3);
+    double distance1 = pointDistance(x2, y2, x3, y3);
+    double distance2 = pointDistance(x1, y1, x3, y3);
+    double distance3 = pointDistance(x1, y1, x2, y2);
+
+    double angle1 = Math.acos((distance1 * distance1 - distance2 * distance2 - distance3 * distance3) / (-2 * distance2 * distance3));
+    double angle2 = Math.acos((distance2 * distance2 - distance1 * distance1 - distance3 * distance3) / (-2 * distance1 * distance3));
+    double angle3 = Math.acos((distance3 * distance3 - distance1 * distance1 - distance2 * distance2) / (-2 * distance1 * distance2));
+
+    angle1 = Math.toDegrees(angle1);
+    angle2 = Math.toDegrees(angle2);
+    angle3 = Math.toDegrees(angle3);
+
+    return "The three angles are: " + angle1 + " " + angle2 + " " + angle3;
+  }
+
+  public static double pointDistance(double x1, double y1, double x2, double y2) {
+  double distance = Math.pow((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)), 0.5);
+  return distance;
   }
 }
