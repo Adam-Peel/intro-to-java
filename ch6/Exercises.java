206e9319766f619304ab18cba44d56851077daa9
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-09-21 20:12:35 +0100

    Add methods for exercises 6.2 and 6.3

--- ch6/Exercises.java
+++ ch6/Exercises.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Exercises {
 
 public static void main (String[] args) {
-pentTest(); 
-
+Scanner input = new Scanner(System.in);
+// pentTest();
+// sumDigits();
+isPalindrome();
 }
 
   public static void pentTest() {
@@ -26,5 +28,48 @@ pentTest();
 public static int getPentagonalNumber(int n) {
   return n * (3 * n - 1)/2;
 }
+
+public static void sumDigits() {
+  Scanner input = new Scanner(System.in);
+  System.out.println("Please enter an integer");
+  int number = input.nextInt();
+  int givenNumber = number;
+  int sum = 0;
+
+  do {
+    int digit = number % 10;
+    sum += digit;
+    number = number / 10;
+  }
+  while (number > 10);
+
+  sum += number;
+  input.close();
+  System.out.println("The sum of the digits in " + givenNumber + " is: " + sum);
+}
+
+  public static int reverse(int number) {
+    int reversed = 0;
+    while (number > 0) {
+        int digit = number % 10;       
+        reversed = reversed * 10 + digit; 
+        number = number / 10;          
+    }
+    System.out.println(reversed);
+    return reversed;
+  }
+
+  public static boolean isPalindrome() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter an integer to check if it is a palindrome");
+    int number = input.nextInt();
+   if (number == reverse(number)){
+    System.out.println("" + number + " Is a palindrome");
+    return true;
+   } else {
+    System.out.println("" + number + " Is not a palindrome");
+    return false;
+   }
+  }
 
 }
